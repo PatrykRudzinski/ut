1e7e5d7c76a7e1f740a1a811a3e3376ef8636d31
patrykrudzinski <patrykrudzinski@gmail.com>
2022-07-21 12:10:39 +0200

    gh action test

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,12 +3,9 @@ import path from "path";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Urszula Turewicz`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "Urszula Turewicz",
+    siteUrl: "https://urszulaturewicz.pl",
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-styled-components",
